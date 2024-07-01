9dbc5b56b5db4c67db151942bbc5b8abd17c2092
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-07-01 10:29:24 -0400

    figured out how scanner works

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-
+//here is where the import for the scanner comes in
 public class Main {
     public static void main(String[] args) {
+        //scanner opens here, and in the "console window" it's expecting a string and then a
+        //number, in that format exactly - it will give you an error code if you input something else
+        //scanner is working as intended
         Scanner scanner = new Scanner(System.in);
         String myString = scanner.next();
         int myInt = scanner.nextInt();
-        myString = "Hi";
-        myInt = 5;
         scanner.close();
 
         System.out.println("my string is: " +myString);
